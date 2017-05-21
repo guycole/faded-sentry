ddef953d892d2c7dc66b9ac10f8173f517ec0a1b
Guy Cole <guycole@gmail.com>
2017-05-21 20:42:21 +0000

    release candidate

---
+++ gpio.py
@@ -0,0 +1,80 @@
+#
+# Title:gpio.py
+# Description:
+# Development Environment:OS X 10.9.3/Python 2.7.7
+# Author:G.S. Cole (guycole at gmail dot com)
+#
+import os
+
+class gpio:
+    gpio_directory = '/sys/class/gpio'
+
+    def __init__(self, port):
+        self.port = port
+
+	self.port_directory = "%s/gpio%d" % (self.gpio_directory, self.port)
+	if not os.path.isdir(self.port_directory):
+		export_file = open("%s/export" % self.gpio_directory, 'w')
+		export_file.write(str(port))
+		export_file.close()
+
+    def getActiveLowValue(self):
+        temp_name = "%s/active_low" % (self.port_directory)
+	temp_file = open(temp_name, 'r')
+	self.gpio_active_low = int(temp_file.readline())
+	temp_file.close()
+	return self.gpio_active_low
+
+    def getDirectionValue(self):
+        temp_name = "%s/direction" % (self.port_directory)
+	temp_file = open(temp_name, 'r')
+	self.gpio_direction = temp_file.readline().strip()
+	temp_file.close()
+	return self.gpio_direction
+
+    def setDirectionValue(self, arg):
+        temp_name = "%s/direction" % (self.port_directory)
+	temp_file = open(temp_name, 'w')
+	temp_file.write(arg)
+	temp_file.close()
+
+    def getEdgeValue(self):
+        temp_name = "%s/edge" % (self.port_directory)
+	temp_file = open(temp_name, 'r')
+	self.gpio_edge = temp_file.readline().strip()
+	temp_file.close()
+	return self.gpio_edge
+
+    def getGpioValue(self):
+        temp_name = "%s/value" % (self.port_directory)
+	temp_file = open(temp_name, 'r')
+	self.gpio_value = int(temp_file.readline())
+	temp_file.close()
+	return self.gpio_value
+
+    def setGpioValue(self, arg):
+        temp_name = "%s/value" % (self.port_directory)
+	temp_file = open(temp_name, 'w')
+	temp_file.write(str(arg))
+	temp_file.close()
+
+print 'start'
+
+#
+if __name__ == '__main__':
+    print 'main'
+
+#    driver = Gpio(60)
+#    print driver.getGpioValue()
+#    print driver.setDirectionValue('out')
+#    print driver.setGpioValue(1)
+
+#    driver = Gpio(5)
+#    print driver.getGpioValue()
+#    print driver.getEdgeValue()
+#    print driver.getDirectionValue()
+#    print driver.getActiveLowValue()
+#    print driver.setDirectionValue('out')
+#    print driver.setGpioValue(0)
+
+print 'stop'
--- perky_blue.py
+++ perky_blue.py
@@ -5,64 +5,48 @@
 # Development Environment:OS X 10.9.3/Python 2.7.7
 # Author:G.S. Cole (guycole at gmail dot com)
 #
-#import time
+import gpio
 
 from bluetooth import *
 
-import Adafruit_BBIO.GPIO as GPIO
-
-class GpioLed:
-    yellowLed = 0
-    greenLed = 0
-    redLed = 0
-
-    def __init__(self):
-        # yellow
-        GPIO.setup("P9_12", GPIO.OUT)
-        GPIO.output("P9_12", GPIO.LOW)
-
-        # green
-        GPIO.setup("P9_15", GPIO.OUT)
-        GPIO.output("P9_15", GPIO.LOW)
-
-        # red 
-        GPIO.setup("P9_23", GPIO.OUT)
-        GPIO.output("P9_23", GPIO.LOW)
-
-    def toggleLed(self, led):
-        if led == 'green':
-            print "green led:%d" % self.greenLed
-            if self.greenLed:
-                GPIO.output('P9_15', GPIO.LOW)
-                self.greenLed = 0
-            else:
-                GPIO.output('P9_15', GPIO.HIGH)
-                self.greenLed = 1
-        elif led == 'red':
-            print "red led:%d" % self.redLed
-            if self.redLed:
-                GPIO.output('P9_23', GPIO.LOW)
-                self.redLed = 0
-            else:
-                GPIO.output('P9_23', GPIO.HIGH)
-                self.redLed = 1
-        elif led == 'yellow':
-            print "yellow led:%d" % self.yellowLed
-            if self.yellowLed == 1:
-                print 'set yellow low'
-                GPIO.output('P9_12', GPIO.LOW)
-                self.yellowLed = 0
-            else:
-                print 'set yellow high'
-                GPIO.output('P9_12', GPIO.HIGH)
-                self.yellowLed = 1
-        else:
-            print("unknown led:%s" % led)
-
 class PerkyBlueServer:
 
+    def toggleLed(self, target):
+        print 'toggle'
+
+	if target == 'green':
+		print 'green'
+		if self.green_led.getGpioValue() < 1:
+                    self.green_led.setGpioValue(0)
+                else:
+                    self.green_led.setGpioValue(1)
+	elif target == 'red':
+		print 'red'
+		if self.red_led.getGpioValue() < 1:
+                    self.red_led.setGpioValue(0)
+                else:
+                    self.red_led.setGpioValue(1)
+        elif target == 'yellow':
+		print 'yellow'
+		if self.yellow_led.getGpioValue() < 1:
+                    self.yellow_led.setGpioValue(0)
+                else:
+                    self.yellow_led.setGpioValue(1)
+	else:
+		print 'unknown remote command'
+
     def execute(self):
-        gpioLed = GpioLed()
+	self.green_led = gpio.gpio(48)
+        self.green_led.setDirectionValue('out')
+        self.green_led.setGpioValue(0)
+
+	self.red_led = gpio.gpio(49)
+        self.red_led.setDirectionValue('out')
+        self.red_led.setGpioValue(0)
+
+	self.yellow_led = gpio.gpio(60)
+        self.yellow_led.setDirectionValue('out')
+        self.yellow_led.setGpioValue(0)
 
         service_uuid = "00001101-0000-1000-8000-00805F9B34FB"
 
@@ -74,19 +58,19 @@ class PerkyBlueServer:
 
         advertise_service(server_sock, "PerkyBlue", service_id = service_uuid, service_classes = [service_uuid, SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
 
-        print("awaiting RFCOMM connection on channel %d" % port)
+        print("awaiting RFCOMM connection on channel:%d" % port)
 
         client_sock, client_info = server_sock.accept()
-        print("accepted connection from ", client_info)
+        print("accepted connection from:", client_info)
 
         try:
             while True:
-                data = client_sock.recv(1024)
+                data = client_sock.recv(1024).strip()
                 if len(data) == 0: break
                 print("received [%s]" % data)
                 client_sock.sendall('OK')
 
-                gpioLed.toggleLed(data)
+                self.toggleLed(data)
         except IOError:
             pass
 
@@ -96,19 +80,10 @@ class PerkyBlueServer:
         server_sock.close()
         print("all done")
 
-
 print 'start'
 
 #
 if __name__ == '__main__':
-#    gpioLed = GpioLed()
-#    gpioLed.toggleLed('yellow')
-#    gpioLed.toggleLed('green')
-#    time.sleep(5)
-#    gpioLed.toggleLed('yellow')
-#    gpioLed.toggleLed('green')
-#    time.sleep(5)
-
     server = PerkyBlueServer()
     server.execute()
 
